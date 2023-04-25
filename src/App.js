5ff6e1b67b25374c5147f0da7eb9866ff9785282
es1564 <111486640+es1564@users.noreply.github.com>
2023-04-25 12:29:07 +0900

    moviedetail 상세내용부르기 완료?

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div>
+    <div className="home-movie">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />}/>
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom'
 import { movieAction } from '../redux/actions/MovieAction'
 import { useDispatch, useSelector } from 'react-redux'
 import ClipLoader from "react-spinners/ClipLoader";
-import { Badge } from 'react-bootstrap'
+import { Badge, Container, Row, Col, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers, faStar } from '@fortawesome/free-solid-svg-icons'
 
 const MovieDetail = () => {
   let {id} = useParams();
@@ -22,40 +24,102 @@ const MovieDetail = () => {
     return <ClipLoader color="#e32636" loading={loading} size={150}/>;
   }
   return (
-    <div>MovieDetail
-      <div>
-        <img
-          src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
-          alt=""
-        />
-      </div>
-      <div >
-        <h1>{movie.title}</h1>
-        <div>
-          {movie.genres.map((genres) => (
-            <Badge bg="danger">
-              {genres.name}
-            </Badge>
-          ))}
-        </div>
-        <div>
-          <span>{movie.vote_average}  </span>
-          <span>{movie.popularity}  </span>
-          <span>{movie.adult ? "청불" : "Under 18"}</span>
-        </div>
-        <div>
-          {movie.overview}
-        </div>
-        <div>
-          <span>Release Day  </span>
-          <span>{movie.release_date}</span>
-        </div>
-        <div>
-          <span>runtime  </span>
-          <span>{movie.runtime}</span>
-        </div>
-      </div>
-    </div>
+    <Container className="detail-container">
+      <Row>
+        <Col className='product-img'>
+          <img
+            src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+            alt=""
+          />
+        </Col>
+        <Col>
+          <div>
+            <ul>
+              <h1>{movie.title}</h1>
+            </ul>
+            <div>
+              <ul>
+              {movie.genres.map((genres) => (
+                <Badge bg="danger">{genres.name}</Badge>
+              ))}
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <span><FontAwesomeIcon icon={faStar} /></span>
+                  <span>  {movie.vote_average}  </span>
+                  <span><FontAwesomeIcon icon={faUsers} /></span>
+                  <span>  {movie.popularity}  </span>
+                  <span>{movie.adult ? "청불" : "Under 18"}</span>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  {movie.overview}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <span><Badge bg="danger">budget</Badge>  </span>
+                  <span>${movie.budget}</span>
+                </li>
+                <li>
+                  <span><Badge bg="danger">revenue</Badge>  </span>
+                  <span>${movie.revenue}</span>
+                </li>
+                <li>
+                  <span><Badge bg="danger">Release Day</Badge>  </span>
+                  <span>{movie.release_date}</span>
+                </li>
+                <li>
+                  <span><Badge bg="danger">runtime</Badge>  </span>
+                  <span>{movie.runtime}</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+    // <div>
+    //   <div>
+    //     <img
+    //       src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+    //       alt=""
+    //     />
+    //   </div>
+    //   <div >
+    //     <h1>{movie.title}</h1>
+    //     <div>
+    //       {movie.genres.map((genres) => (
+    //         <Badge bg="danger">
+    //           {genres.name}
+    //         </Badge>
+    //       ))}
+    //     </div>
+    //     <div>
+    //       <span>{movie.vote_average}  </span>
+    //       <span>{movie.popularity}  </span>
+    //       <span>{movie.adult ? "청불" : "Under 18"}</span>
+    //     </div>
+    //     <div>
+    //       {movie.overview}
+    //     </div>
+    //     <div>
+    //       <span>Release Day  </span>
+    //       <span>{movie.release_date}</span>
+    //     </div>
+    //     <div>
+    //       <span>runtime  </span>
+    //       <span>{movie.runtime}</span>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
